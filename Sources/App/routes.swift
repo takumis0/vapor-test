9b6b6c7ce0786581a45141de404b2f6ed216f3ce
takumis0 <33518225+takumis0@users.noreply.github.com>
2019-10-14 10:20:36 +0900

    change return value

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,55 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        let api = {
+          "meta": {
+            "code": 200
+          },
+          "data": [
+            {
+              "question": "Q1. There is a house, One Enters it blind and comes out seeing. What is it?",
+              "answer1": "School",
+              "answer2": "Wearhouse",
+              "answer3": "Restaurant",
+              "answer4": "Karaoke",
+              "correctAnswerNumber": "1"
+            },
+            {
+              "question": "Q2. What goes on four legs in the morning, on two legs at noon, and three legs in the evening?",
+              "answer1": "Watch",
+              "answer2": "Spider",
+              "answer3": "Human",
+              "answer4": "Flower",
+              "correctAnswerNumber": "3"
+            },
+            {
+              "question": "Q3. As I was going to St. Ives, I met a man with seven wives, Each wife had seven sacks, Each sack had seven cats, Each cat had seven kits: Kits, cats, sacks, and wives, How many were there going to St. Ives?",
+              "answer1": "One",
+              "answer2": "Two",
+              "answer3": "Three",
+              "answer4": "Four",
+              "correctAnswerNumber": "1"
+            },
+            {
+              "question": "Q4. There is a house, One Enters it blind and comes out seeing. What is it?",
+              "answer1": "School",
+              "answer2": "Wearhouse",
+              "answer3": "Restaurant",
+              "answer4": "Karaoke",
+              "correctAnswerNumber": "1"
+            },
+            {
+              "question": "Q5. What goes on four legs in the morning, on two legs at noon, and three legs in the evening?",
+              "answer1": "Watch",
+              "answer2": "Spider",
+              "answer3": "Human",
+              "answer4": "Flower",
+              "correctAnswerNumber": "3"
+            }
+          ]
+        }
+                
+        return api
     }
     
     // Basic "Hello, world!" example
